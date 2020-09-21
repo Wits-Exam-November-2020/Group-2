a995ab476919bfa9c41b1066cef21ff7c24448e3
BrentonBudler <60913244+BrentonBudler@users.noreply.github.com>
2020-09-21 14:59:14 +0200

    Flying enemy

--- Assets/Brenton_Budler/Scripts/EnemyController.cs
+++ Assets/Brenton_Budler/Scripts/EnemyController.cs
@@ -18,6 +18,7 @@ public class EnemyController : MonoBehaviour
     private bool isAttacking = false;
 
     private Vector3 playerLoc;
+    
 
     private void Start()
     {
@@ -32,40 +33,60 @@ public class EnemyController : MonoBehaviour
     {
         player = GameObject.Find("Player(Clone)");
         target = GameObject.Find("Player(Clone)").transform; 
-        enemy.SetDestination(target.position);
+      
 
 
+        //   enemy.agentTypeID = -334000983;
+        // finalDestination = new Vector3(target.position.x, target.position.y + 3, target.position.z);
 
         //transform.Translate(0,1f,0);
 
-        if (gameObject.tag=="FlyingEnemy1")
-        {
+        //if (gameObject.tag=="FlyingEnemy1")
+        //{
 
 
 
-            transform.localPosition = Vector3.Lerp(transform.localPosition, new Vector3(transform.localPosition.x, player.transform.position.y, transform.localPosition.z), 0.5f * Time.deltaTime);
+        //    transform.localPosition = Vector3.Lerp(transform.localPosition, new Vector3(transform.localPosition.x, player.transform.position.y, transform.localPosition.z), 0.5f * Time.deltaTime);
 
-
-
-            playerLoc.x = player.transform.position.x;
-            playerLoc.z = player.transform.position.z;
-            playerLoc.y = enemy.transform.position.y;
-
-            if (Vector3.Distance(enemy.transform.position, playerLoc) <= enemy.stoppingDistance)
-            {
-                if (!isAttacking)
-                {
-                    isAttacking = true;
-                    player.GetComponent<Player>().TakeDamage(damage);
-                    Invoke("ResetAttack", 2);
-
-
-                }
-                Debug.Log("Attack");
-            }
+        if (player.transform.position.y>10) {
+            enemy.enabled = false;
+            //   transform.localPosition = Vector3.Lerp(transform.localPosition, player.transform.position, 0.5f * Time.deltaTime);
+           // if (Vector3.Distance(enemy.transform.position, player.transform.position) <= 10)
+           // {
+                transform.position = Vector3.Lerp(transform.position, player.transform.position, 1f * Time.deltaTime);
+           // }
+           
         }
         else
         {
+            transform.position = Vector3.Lerp(transform.position, new Vector3(0,1,0), 0.5f * Time.deltaTime);
+            enemy.enabled = true;
+            enemy.SetDestination(target.position);
+        }
+        
+
+
+
+
+        //    playerLoc.x = player.transform.position.x;
+        //    playerLoc.z = player.transform.position.z;
+        //    playerLoc.y = enemy.transform.position.y;
+
+        //    if (Vector3.Distance(enemy.transform.position, playerLoc) <= enemy.stoppingDistance)
+        //    {
+        //        if (!isAttacking)
+        //        {
+        //            isAttacking = true;
+        //            player.GetComponent<Player>().TakeDamage(damage);
+        //            Invoke("ResetAttack", 2);
+
+
+        //        }
+        //        Debug.Log("Attack");
+        //    }
+        //}
+        //else
+        //{
             if (Vector3.Distance(enemy.transform.position, target.position) <= enemy.stoppingDistance)
             {
                 if (!isAttacking)
@@ -79,7 +100,7 @@ public class EnemyController : MonoBehaviour
                 Debug.Log("Attack");
             }
 
-        }
+      //  }
 
 
 
--- Assets/HeightMatcher.cs
+++ Assets/HeightMatcher.cs
@@ -8,24 +8,24 @@ public class HeightMatcher : MonoBehaviour
 {
 
     private GameObject player;
-    private NavMeshAgent enemy;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        enemy = this.GetComponent<NavMeshAgent>();
-    }
 
     // Update is called once per frame
     void Update()
     {
         player = GameObject.Find("Player(Clone)");
 
-       // transform.position = new Vector3(transform.position.x, player.transform.position.y+1, transform.position.z);
+        // transform.position = new Vector3(transform.position.x, player.transform.position.y+1, transform.position.z);
 
 
-        transform.position = Vector3.Lerp(transform.position, new Vector3(transform.position.x, player.transform.position.y, transform.position.z), 1f);
+        // transform.position = Vector3.Lerp(transform.position, new Vector3(transform.position.x, player.transform.position.y+1, transform.position.z), 0.5f * Time.deltaTime);
 
-
+        //transform.localPosition = Vector3.Lerp(transform.localPosition, new Vector3(transform.localPosition.x, player.transform.position.y, transform.localPosition.z), 0.5f * Time.deltaTime);
+        //if (player.transform.position.y>20) {
+        //    transform.position = new Vector3(transform.position.x, player.transform.position.y + 1, transform.position.z);
+        //}
+        //else
+        //{
+            
+        //}
     }
 }
