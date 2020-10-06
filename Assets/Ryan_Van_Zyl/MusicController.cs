50a70312b3cbd93db5cc5d4c5cedcfd2e967a55d
Ryan Van Zyl <64086096+Ryan-rvz@users.noreply.github.com>
2020-10-06 18:29:31 +0200

    SoundFixes + Particle Effects

---
+++ Assets/Ryan_Van_Zyl/MusicController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicController : MonoBehaviour
+{
+    private void Awake()
+    {
+
+        DontDestroyOnLoad(this.gameObject);
+
+        if (FindObjectsOfType(GetType()).Length > 1)
+        {
+            Destroy(gameObject);
+        }
+
+    }
+}
