6ee7a01d7fa77316c247da69b3dc9a5f321a9e4b
BrentonBudler <60913244+BrentonBudler@users.noreply.github.com>
2020-11-20 13:57:27 +0200

    Boss Enemy Movement

---
+++ Assets/FollowWayPoints.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowWayPoints : MonoBehaviour
+{
+    [SerializeField]
+    private Transform[] waypoints;
+
+    [SerializeField]
+    private float moveSpeed = 2f;
+
+    private int waypointIndex = 0;
+
+    private void Start()
+    {
+        transform.position = waypoints[waypointIndex].transform.position; 
+
+    }
+
+    private void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position,
+         waypoints[waypointIndex].transform.position,
+         moveSpeed * Time.deltaTime);
+
+        if (transform.position == waypoints[waypointIndex].transform.position)
+        {
+            if (waypointIndex + 1 < waypoints.Length)
+            {
+                waypointIndex += 1;
+            }
+            else
+            {
+                waypointIndex = 0;
+            }
+            
+        }
+    }
+
+
+}
