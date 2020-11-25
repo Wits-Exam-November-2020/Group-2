ec37f26b4b0987dfbb7656b1890add9b48d52f95
Ryan Van Zyl <64086096+Ryan-rvz@users.noreply.github.com>
2020-11-25 15:00:03 +0200

    BossExplosion

--- Assets/Ryan_Van_Zyl/BossDamageEffects.cs
+++ Assets/Ryan_Van_Zyl/BossDamageEffects.cs
@@ -7,6 +7,8 @@ public class BossDamageEffects : MonoBehaviour
 
     private BossController bController;
     public GameObject sEffect1, sEffect2, sEffect3, sEffect4;
+    public GameObject explode1, explode2, explode3, explode4;
+    public GameObject body1, body2, body3, body4;
     private float maxHealth;
 
     // Start is called before the first frame update
@@ -19,6 +21,18 @@ public class BossDamageEffects : MonoBehaviour
         sEffect2.SetActive(false);
         sEffect3.SetActive(false);
         sEffect4.SetActive(false);
+
+        explode1.SetActive(false);
+        explode2.SetActive(false);
+        explode3.SetActive(false);
+        explode4.SetActive(false);
+
+        body1.SetActive(false);
+        body2.SetActive(false);
+        body3.SetActive(false);
+        body4.SetActive(false);
+
+
     }
 
     // Update is called once per frame
@@ -39,6 +53,19 @@ public class BossDamageEffects : MonoBehaviour
         if (bController.health <= (maxHealth/3.3))
         {
             sEffect4.SetActive(true);
+        }
+
+        if(bController.health <= 0)
+        {
+            explode1.SetActive(true);
+            explode2.SetActive(true);
+            explode3.SetActive(true);
+            explode4.SetActive(true);
+
+            body1.SetActive(true);
+            body2.SetActive(true);
+            body3.SetActive(true);
+            body4.SetActive(true);
         }
     }
 }
